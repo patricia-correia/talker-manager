019a3e1c4afa0a0ff7c29f6db585e3098d27419a
Patricia Correia <pncorreia25@gmail.com>
2022-10-04 17:05:08 -0300

    criando o endpoit com get/talker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readTalkersData } = require('./utils/fsUtils.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+  const talkers = await readTalkersData();
+  response.status(HTTP_OK_STATUS).json(talkers);
 });
 
 app.listen(PORT, () => {
